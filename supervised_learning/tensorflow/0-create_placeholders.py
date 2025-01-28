d15f59761b0871dc1e275c579f6223929331de65
Jules-gatete <j.gatete@alustudent.com>
2025-01-28 11:27:53 +0200

    first file updated

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -7,8 +7,6 @@ import tensorflow as tf
 
 def create_placeholders(nx, classes):
     """Creates placeholders for neural network"""
-
     x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
     y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
-
     return x, y
---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that creates a layer for neural network
+"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """Creates a layer for neural network"""
+    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        name='layer'
+    )
+    return layer(prev)
