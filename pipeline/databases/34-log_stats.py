2ee38f8b547a2770024bc4c11c65b25608bdbc01
Jules-gatete <j.gatete@alustudent.com>
2025-03-08 23:13:55 +0200

    database

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
-"""script that provides some stats about Nginx logs stored in MongoDB"""
-
+"""Script that provides some stats about Nginx logs stored in MongoDB"""
 
 from pymongo import MongoClient
 
-
 if __name__ == '__main__':
+    try:
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        collection = client.logs.nginx
 
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+        # Total number of documents
+        total_logs = collection.count_documents({})
 
-    # total number of documents
-    total_logs = collection.count_documents({})
+        methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+        method_counts = {method: collection.count_documents({"method": method}) for method in methods}
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: collection.count_documents(
-        {"method": method}) for method in methods}
+        # Count of status check
+        status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
 
-    # count of status check
-    status_check_count = collection.count_documents(
-        {"method": "GET", "path": "/status"})
+        # Print output
+        print(f"{total_logs} logs")
+        print("Methods:")
+        for method in methods:
+            print(f"\tmethod {method}: {method_counts[method]}")
+        print(f"{status_check_count} status check")
 
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check_count} status check")
+    except Exception as e:
+        print(f"Error: {e}")
