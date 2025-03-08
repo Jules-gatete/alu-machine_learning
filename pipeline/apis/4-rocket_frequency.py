3646f998d97cb7bf8a8c51f1486a58eb244a993c
Jules-gatete <j.gatete@alustudent.com>
2025-03-08 22:33:15 +0200

    correct output

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -18,8 +18,14 @@ if __name__ == '__main__':
         else:
             rocket_dict[rocket_id] = 1
 
+    # Define a specific order for rocket names
+    rocket_order = ["5e9d0d95eda69955f709d1eb",  # Falcon 9
+                    "5e9d0d95eda69974e44b38c5",  # Falcon Heavy
+                    "5e9d0d95eda69955f709d1ec"]  # Falcon 1
+
     # Sort and print the rocket names and their counts
-    for key, value in sorted(rocket_dict.items(), key=lambda kv: kv[1], reverse=True):
-        rurl = "https://api.spacexdata.com/v4/rockets/" + key
-        req = requests.get(rurl)
-        print(req.json()["name"] + ": " + str(value))
+    for key in rocket_order:
+        if key in rocket_dict:
+            rurl = "https://api.spacexdata.com/v4/rockets/" + key
+            req = requests.get(rurl)
+            print(req.json()["name"] + ": " + str(rocket_dict[key]))
