06ee4c12f1fe86f41c415bea45caf36353c9c1c0
Jules-gatete <j.gatete@alustudent.com>
2025-07-01 00:15:24 +0200

    updated

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -35,7 +35,7 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         return None, None
     if kmax is None:
         kmax = X.shape[0]
-    if kmin > kmax:
+    if kmax < kmin:
         return None, None
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
